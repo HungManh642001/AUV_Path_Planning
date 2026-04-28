4ac6cd699babd11af6e2af13c2370443c310fd4f
ManhNgoHung <82172471+HungManh642001@users.noreply.github.com>
2026-04-28 08:30:37 +0100

    Update configuration parameters in config.py

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
 N = 100
-k = 2.0
+k = 5.0
 alpha = 45.0
-beta = 60
-L_max = 800.0
+beta = 60.0
+L_max = 1000.0
 L_min = 400.0
-l = 100.0
+l = 300.0 # d_ss
 p = 0.01
 n_particles = 300
 scan_size = 40
-entropy_threshold = 9.1
+entropy_threshold = 9.1 
+max_iterations = 5000
+
+# Flight contraints
+l_min: float = 400
+a_max_deg: float = 90.0
+R: float = 300.0
+d_ss: float = 300.0
